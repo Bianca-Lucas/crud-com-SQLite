45a542cbed44ca11bdaae1338b07b79928bba514
Bianca Lucas <biaxx.iluks@gmail.com>
2025-05-06 15:32:30 -0300

    feat: add middleware

---
+++ src/middleware/validate.js
@@ -0,0 +1,24 @@
+// -> Referência de Middleware:
+//    Obs: sempre vai os 3 /--|--\
+//                        /   |   \
+export function validate(schema){
+    return (req, res, next) => {
+        try {
+        // -> Valida o corpo da requisição contra o schema:
+        const validatedData = schema.parse(req.body)
+        
+        // -> Substituir o body pelos dados fornecidos:
+        req.body = validatedData
+
+        // -> Chamo o próximo agente (middleware):
+        next()
+
+        } catch (error) {
+            res.status(400).json({
+                messagem: 'Erro de validação!',
+                erro: error.message
+            })
+
+
+    }}
+}
